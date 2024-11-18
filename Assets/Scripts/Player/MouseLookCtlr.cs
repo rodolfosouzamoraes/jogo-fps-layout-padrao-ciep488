5b0b55fd25ff68e296d2a8440d20615115830d37
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2024-11-18 08:44:45 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/Player/MouseLookCtlr.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+/// <summary>
+/// Classe responsável por movimentar a camera do jogador
+/// </summary>
+public class MouseLookCtlr : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    Vector2 lastAxis;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LockAndHideMouse();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;   
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f,60f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up*mouseX);
+
+        HideOrShowMouse();
+    }
+
+    /// <summary>
+    /// Exibe ou oculta o mouse
+    /// </summary>
+    void HideOrShowMouse(){
+        /*if(PauseGameMng.Instance.IsPause){
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else{
+            LockAndHideMouse();
+        }*/
+    }
+
+    void LockAndHideMouse(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovimentCtlr.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+/// <summary>
+/// Classe responsável por movimentar o jogador
+/// </summary>
+public class PlayerMovimentCtlr : MonoBehaviour
+{
+    CharacterController controller;  
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance =0.4f;
+    public LayerMask groundMask;
+    public bool isEnableJum = true;
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(PauseGameMng.Instance.IsPause) return;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(move * speed * Time.deltaTime);
+        
+        if(Input.GetAxis("Jump")>0 && isGrounded && isEnableJum){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f *gravity);
+        }
+
+        velocity.y += gravity *Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+}
